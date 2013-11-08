d68151e3e5013132e1be696744b2a51839d8eadd
Luke Maurits <luke@maurits.id.au>
2013-11-08 15:23:01 -0800

    Added dinky demo tree nonsense.

---
+++ TreeBuilder/make_demo_trees.py
@@ -0,0 +1,42 @@
+# Super gross script to make some demonstration IE trees with
+# human friendly names
+
+import glob
+from fileio import load_translate_file
+
+revtrans = load_translate_file("generated_trees/indo.translate")
+names = revtrans.keys()
+trans = {}
+for name in names:
+    trans[revtrans[name]] = name
+
+for i in range(1,11):
+    fp = open("generated_trees/combination/indo/tree_%d.tree" % i, "r")
+    string = fp.read()
+    fp.close()
+    for number in trans:
+        print "Replacing ", number, " with ", trans[number]
+        string = string.replace("(%d:" % number, "(%s:" % trans[number])
+        string = string.replace(",%d:" % number, ",%s:" % trans[number])
+    fp = open("demo_trees/demo_indo_%d.tree" % i, "w")
+    fp.write(string)
+    fp.close()
+
+backtrans = load_translate_file("generated_trees/indo.translate")
+trans = {}
+for key in backtrans:
+    trans[backtrans[key]] = key
+
+for filename in glob.glob("demo_trees/*.tree"):
+    fp = open(filename, "r")
+    text = fp.read()
+    fp.close()
+
+    for number in trans:
+        text = text.replace("(%d:" % number, "(%s:" % trans[number])
+        text = text.replace(",%d:" % number, ",%s:" % trans[number])
+
+    fp = open(filename.replace(".tree","_named.tree"), "w")
+    fp.write(text)
+    fp.close()
+
